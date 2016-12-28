0ef3bab506b184b00b5a1bc5a2afc3f9956a79fd
sirrah23 <hseth93@gmail.com>
2016-12-27 21:49:30 -0500

    Added two players

--- sketch.js
+++ sketch.js
@@ -4,22 +4,30 @@ var P1;
 function setup() {
   createCanvas(800, 640);
   P1 = new Player(50, 25);
+  P2 = new Player(50, height - 75);
 }
 
 function draw() {
   background(0);
   P1.show();
+  P2.show();
 }
 
 function keyPressed(){
   if (keyCode === LEFT_ARROW){
-    console.log("YO");
     P1.setSpeed(-10);
   } else if (keyCode === RIGHT_ARROW){
     P1.setSpeed(10);
+  }
+
+  if (keyCode === 65){
+    P2.setSpeed(-10);
+  } else if (keyCode === 68){
+    P2.setSpeed(10);
   }
 }
 
 function keyReleased(){
   P1.setSpeed(0);
+  P2.setSpeed(0);
 }
