807ea8a9a831a23443065a53c3b844b2ec430f39
sirrah23 <hseth93@gmail.com>
2016-12-29 02:36:03 -0500

    Ball exists now and can be played with

--- app.js
+++ app.js
@@ -8,18 +8,34 @@ var P2Socket; // Player Two
 var guestSockets = []; // People Watching
 
 var width = 800;
+var height = 640;
+
+var playerWidth = width/2
+var playerHeight = 45
+
+//TODO: Create full fledged player and ball objects
 
 var P1Pos = {
     "x" : 50,
     "y" : 25
 };
+
 var P2Pos = {
     "x" : 50,
-    "y" : 565
+    "y" : height - 75,
+}
+
+var ballPos = {
+  x: width/2,
+  y: height/2
 }
 
 var P1Speed = 0;
 var P2Speed = 0;
+var ballSpeed = {
+  x: -10,
+  y: -10
+};
 
 var width = 800;
 var playerWidth = 400;
@@ -52,7 +68,9 @@ io.sockets.on('connection', function(socket){
     "P1x" : P1Pos.x,
     "P1y" : P1Pos.y,
     "P2x" : P2Pos.x,
-    "P2y" : P2Pos.y
+    "P2y" : P2Pos.y,
+    "ballX" : ballPos.x,
+    "ballY" : ballPos.y
   };
 
   socket.emit("connectionSetup", data)
@@ -68,11 +86,25 @@ io.sockets.on('connection', function(socket){
   setInterval(function(){
       P1Pos.x += P1Speed;
       P2Pos.x += P2Speed;
+      if (ballGoal()){
+        ballReset();
+      } else if (ballCollisionSides()){
+        ballSpeed.x *=-1
+      } else if(ballCollisionPlayer(P1Pos)){
+          ballCollisionPlayerReact(P1Pos);
+      } else if (ballCollisionPlayer(P2Pos)){
+          ballCollisionPlayerReact(P2Pos);
+      }
+      ballPos.x += ballSpeed.x
+      ballPos.y += ballSpeed.y
+      ballCollisionPlayer(P1Pos)
       var data = {
         "P1x" : P1Pos.x,
         "P1y" : P1Pos.y,
         "P2x" : P2Pos.x,
-        "P2y" : P2Pos.y
+        "P2y" : P2Pos.y,
+        "ballX" : ballPos.x,
+        "ballY" : ballPos.y
       };
       checkPos();
       socket.emit("updateScreen", data);
@@ -86,4 +118,49 @@ function checkPos(){
   }else if (P1Pos.x > width){
     P1Pos.x = width - playerWidth;
   }
+}
+
+function ballCollisionPlayer(player){
+  if (ballPos.x < player.x || ballPos.x > (player.x + playerWidth)
+    || ballPos.y < player.y || ballPos.y > (player.y + playerHeight)){
+      return false;
+    }
+    return true;
+}
+
+function ballCollisionSides(){
+  return ballPos.x < 0 || ballPos.x > width
+}
+
+function ballGoal(){
+  return ballPos.y < 0 || ballPos.y > height
+}
+
+function ballReset(){
+
+  ballPos = {
+    x: width/2,
+    y: height/2
+  }
+
+  ballSpeed = {
+    x : -10,
+    y : 10
+  }
+}
+
+function ballCollisionPlayerReact(player){
+    ballSpeed.y *= -1;
+    if (ballPos.x < player.x + playerWidth/2){
+        ballSpeed.x = abs(ballSpeed.x) * -1
+    } else {
+        ballSpeed.x = abs(ballSpeed.x);
+    }
+}
+
+function abs(n){
+  if(n < 0){
+    return n*-1;
+  }
+  return n;
 }
--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,6 @@
 var P1;
 var P2;
+var ball;
 var socket;
 var isPlayer;
 var playerNum;
@@ -12,16 +13,15 @@ function setup() {
   socket.on("connectionSetup", function(data){
     isPlayer = data.isPlayer;
     playerNum = data.playerNum;
-    console.log("YO")
     P1 = new Player(data.P1x, data.P1y);
     P2 = new Player(data.P2x, data.P2y);
-    console.log(P1);
-    console.log(P2);
+    ball = createVector(data.ballX, data.ballY);
   });
 
   socket.on("updateScreen", function(data){
     P1.update(data.P1x, data.P1y);
     P2.update(data.P2x, data.P2y);
+    ball = createVector(data.ballX, data.ballY);
   });
 }
 
@@ -31,6 +31,10 @@ function draw() {
     P1.show();
   if(P2)
     P2.show();
+  rectMode("CENTER");
+  if(ball){
+    rect(ball.x, ball.y, 25, 25);
+  }
 }
 
 function keyPressed(){
